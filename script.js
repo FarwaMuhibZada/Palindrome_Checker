deca16f4c43a63f574d2ca9e6cd62faaeb6d61f5
farwaMuhibzada <farwafarid2017@gmail.com>
2024-06-22 15:37:38 +0430

    fixed the ESLint errors .

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const textInput = document.getElementById('text-input');
 const checkButton = document.getElementById('check-btn');
 const result = document.getElementById('result');
-checkButton.addEventListener('click', ()=> {
+checkButton.addEventListener('click', () => {
   const lowerReplaced = textInput.value.toLowerCase().replace(/[^A-Za-z0-9]/g, '');
   if (textInput.value === '') {
     alert('Please input a value');
@@ -10,16 +10,11 @@ checkButton.addEventListener('click', ()=> {
   else if (textInput.value.length === 1) {
     result.innerText = `${textInput.value} is a palindrome`;
     textInput.value = '';
-  }
-
-  else if (lowerReplaced === [...lowerReplaced].reverse().join("")) {
+  }else if (lowerReplaced === [...lowerReplaced].reverse().join('')) {
     result.innerText = `${textInput.value} is a palindrome`;
-    textInput.value = "";
-  }
-
-  else {
+    textInput.value = '';
+  }else {
     result.innerText = `${textInput.value} is not a palindrome`;
-    textInput.value = "";
+    textInput.value = '';
   }
-
 });
