ab33f6e599822c765d6cfc68642e27f49567495d
farwaMuhibzada <farwafarid2017@gmail.com>
2024-06-22 14:59:01 +0430

    added condition for input to show an alert box on script file.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const textInput = document.getElementById('text-input');
-
 const checkButton = document.getElementById('check-btn');
-
 const result = document.getElementById('result');
+checkButton.addEventListener("click",()=>{
+  const lowerReplaced = textInput.value.toLowerCase().replace(/[^A-Za-z0-9]/g,"");
+  if (textInput.value === ""){
+    alert("Please input a value");
+  }
+});
